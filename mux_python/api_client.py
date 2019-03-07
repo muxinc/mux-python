a466952ab2a690bf38448f7fa538720c05f81794
Phil Cluff <philcluff@googlemail.com>
2019-03-06 18:18:01 -0800

    Fix to User-Agent generation

--- mux_python/api_client.py
+++ mux_python/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/1.0.0/python'
+        self.user_agent = 'Mux Python | 1.0.0'
 
     def __del__(self):
         if self._pool:
