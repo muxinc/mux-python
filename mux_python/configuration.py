c4c52b47e96f040c415712fb710915252543a466
Phil Cluff <578330+philcluff@users.noreply.github.com>
2023-10-13 16:11:03 +0100

    Release 3.11.2 (#68)

--- mux_python/configuration.py
+++ mux_python/configuration.py
@@ -406,7 +406,7 @@ conf = mux_python.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 3.11.0".\
+               "SDK Package Version: 3.11.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
