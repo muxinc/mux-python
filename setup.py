7134f63f5263c6fdec784abd2df1409a66d8c99f
Justin Sanford <justin.h.sanford@gmail.com>
2026-01-08 13:56:42 -0800

    update deprecated getHeader method

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "mux_python"
-VERSION = "5.1.1"
+VERSION = "5.1.2"
 # To install the library, run the following
 #
 # python setup.py install
